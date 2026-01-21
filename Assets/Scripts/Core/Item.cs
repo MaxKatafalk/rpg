78aa7389ba40b598706b13bbfcab610f8269afb1
MaxKatafalk <max20060602@mail.ru>
2026-01-21 17:59:14 +1000

    Созданы классы Item и Player

---
+++ Assets/Scripts/Core/Item.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Core;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private string itemName = "Предмет";
+    private string description = "Описание";
+
+    public string GetName()
+    {
+        return itemName;
+    }
+
+    public string GetDescription()
+    {
+        return description;
+    }
+
+    public virtual void Use(Player player)
+    {
+        Debug.Log("Предмет использован: " + itemName);
+    }
+}
---
+++ Assets/Scripts/Core/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Assets.Scripts.Core
+{
+    public class Player : MonoBehaviour
+    {
+        private int health = 100;
+        private int attack = 10;
+        private List<Item> inventory = new List<Item>();
+
+        public int GetHealth()
+        {
+            return health;
+        }
+
+        public int GetAttack()
+        {
+            return attack;
+        }
+
+        public bool IsAlive()
+        {
+            return health > 0;
+        }
+
+        void Start()
+        {
+            Debug.Log("Игрок создан. Здоровье: " + health);
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (!IsAlive()) return;
+
+            health -= damage;
+            if (health < 0) health = 0;
+
+            Debug.Log("Игрок получил урон: " + damage + ". Здоровье: " + health);
+
+            if (!IsAlive())
+            {
+                Debug.Log("Игрок умер!");
+            }
+        }
+
+        public void Heal(int healAmount)
+        {
+            if (!IsAlive()) return;
+
+            health += healAmount;
+            if (health > 100) health = 100;
+
+            Debug.Log("Игрок вылечен на: " + healAmount + ". Здоровье: " + health);
+        }
+        public List<Item> GetInventory()
+        {
+            return inventory;
+        }
+        public void AddItem(Item item)
+        {
+            if (item == null) return;
+
+            inventory.Add(item);
+            Debug.Log("Предмет добавлен в инвентарь: " + item.GetName());
+        }
+
+        
+    }
+}
